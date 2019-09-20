0d66f72d1c313c7279b8430c74daed7158b4579e
Gerard <gerard.riera@clariba.com>
2019-09-20 16:37:56 +0200

    Initial Commit

---
+++ trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trivia
+//
+//  Created by Gerard Riera Puig on 20/09/2019.
+//  Copyright © 2019 Gerard Riera Puig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
